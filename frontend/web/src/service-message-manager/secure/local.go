f74516edcfaa0818fd2bb4da71d423ab86ef35f3
alazhik10d <alazhik10d@users.noreply.github.com>
2025-11-14 13:53:27 +0000

    Add core.css, local.go, user-service.sol, src-adapter.scss

---
+++ frontend/web/src/service-message-manager/secure/local.go
@@ -0,0 +1,4 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/gigmarket_fhe/frontend/web/src/service-message-manager/secure/local.go
+// Generated at: 2025-11-27T15:10:54.521Z
+
+
